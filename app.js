5e77cd0c2a60a0b2ac294e38a961952b97b3790e
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-06 14:07:17 -0700

    test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-var exphbs = require('express-handlebars');
+    const express = require('express')
+    const app = express()
+    var exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    app.get('/', (req, res) => {
 
-app.get('/', (req, res) => {
-    res.render('home', {msg: 'Hello World!'});
-})
+        res.render('home', {msg: 'Hello World!'});
+    })
 
-app.listen(3000, () => {
-    console.log('App Listening on port 3000!')
-})
+    app.listen(3000, () => {
+        console.log('App Listening on port 3000!')
+
+    })
