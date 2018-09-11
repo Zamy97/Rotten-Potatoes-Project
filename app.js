f124733d7e24c62ebec43393260442c22e342466
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-10 22:59:35 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -5,19 +5,28 @@
     app.engine('handlebars', exphbs({defaultLayout: 'main'}));
     app.set('view engine', 'handlebars');
 
-   // OUR MOCK ARRAY OF OBJECTS
-    let reviews = [
-
-        {title: "Great Review"},
-        {title: "Next Review"}
-    ]
+   // // OUR MOCK ARRAY OF OBJECTS
+   //  let reviews = [
+   //
+   //      {title: "Great Review"},
+   //      {title: "Next Review"}
+   //  ]
     // INDEX
     app.get('/reviews', (req, res) => {
+        Review.find().then(reviews => {
             res.render('review-index', { reviews: reviews });
+        })
+            .catch(error => {
+                console.log(error);
+            })
     })
 
     const mongoose = require('mongoose');
     mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+    const Review = mongoose.model('Review', {
+        title: String
+    });
 
 
 
