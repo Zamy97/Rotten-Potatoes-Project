b9bbf10c08c63d6d9222389a1e93e0f899cdfc89
Akhtar Zaman <auz.zamy@gmail.com>
2018-09-14 00:13:44 -0700

    making changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let express = require('express');
-let exphbs = require('express-handlebars');
-let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
-let app = express();
-let reviewController = require('./controllers/reviews');
-let port = process.env.PORT || 5000;
+# https://github.com/IkeyBenz/RottenPotatoes
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();
+const reviewController = require('./controllers/reviews');
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
